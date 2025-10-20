5387fb3b0782c51881a1964e99581c8e3b1e3b25
darshan-gowdaa <darshangowdaa223@gmail.com>
2025-10-20 14:06:46 +0530

    githubpages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Darshan-Gowdaa-Portfolio/',
+  base: '/darshan-gowdaa/',
   plugins: [
     react(),
     tailwindcss(),
